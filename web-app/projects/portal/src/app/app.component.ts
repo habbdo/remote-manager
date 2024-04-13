c1ab1bd0df756110897798e76d25fd97fe62d06e
Abdellah HOUSNI <abdellah.housni@protonmail.com>
2024-04-13 13:58:25 +0200

    first commit

---
+++ web-app/projects/portal/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+import { ToolbarComponent } from '@app/ui';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, ToolbarComponent],
+  template: `
+    <ui-toolbar />
+    <router-outlet />
+  `,
+})
+export class AppComponent {
+  
+}
---
+++ web-app/projects/ui/src/lib/components/toolbar/toolbar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ui-toolbar',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>toolbar works!</p>
+  `
+})
+export class ToolbarComponent {
+
+}
