0f009f168b7942184df8e6034029a483f9b70903
Abdellah HOUSNI <abdellah.housni@protonmail.com>
2024-04-20 14:57:41 +0200

    feat(customer): add customer list
    #13

--- web-app/projects/portal/src/app/pages/customers.page.ts
+++ web-app/projects/portal/src/app/pages/customers.page.ts
@@ -1,16 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
 
 import { DataTableComponent } from '@app/ui';
+import { ListDataProvider } from '@app/providers/list.data-provider';
+import { FakeCustomerListDataProvider } from '@app/providers/fake-customer-list.data.provider';
 
 @Component({
   selector: 'app-customers',
   standalone: true,
-  imports: [DataTableComponent],
+  imports: [AsyncPipe, DataTableComponent],
+  providers: [
+    { provide: ListDataProvider, useClass: FakeCustomerListDataProvider}
+  ],
   template: `
-    <ui-data-table />
-  `,
-  styles: ``
+    <ui-data-table
+      [columns]="listDataProvider.columns"
+      [list]="listDataProvider.list()"
+      (load)="listDataProvider.load($event)"
+    />
+  `
 })
 export class CustomersPage {
-
+  listDataProvider = inject(ListDataProvider);
 }
